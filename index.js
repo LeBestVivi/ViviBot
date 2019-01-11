92ee5d3a488461cbf33a9ca6458b968d2b51d32c
LeBestVivi <46608329+LeBestVivi@users.noreply.github.com>
2019-01-11 20:51:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,138 +1,151 @@
-const Discord = require('discord.js');
-const fs = require('fs')
-const client = new Discord.Client();
-let prefix = "!";
- 
-client.login('NTMzMzQ2OTkzNTc4MzExNjkw.DxptqQ.s2zxQrmocApmK-SJ94yV3_8Em0g');
- 
-client.on('message', message =>{
-    if(message.content === "tu fais quoi?"){
-        message.reply('Je regarde une vidéo de Vivi !');
-        console.log('répond à tfq');
-    }
-});
- 
- 
-client.on('guildMemberAdd', member =>{
-    let embed = new Discord.RichEmbed()
-        .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
-        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('ID DU CHANNEL').send(embed)
-    member.addRole('ID DU ROLE A AJOUTER AUTOMATIQUEMENT')
- 
-});
- 
-client.on('guildMemberRemove', member =>{
-    let embed = new Discord.RichEmbed()
-        .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
-        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('ID DU CHANNEL').send(embed)
- 
-}); 
+const zlib = require('zlib');
+const http = require('http');
+const https = require('https');
+const URL = require('url');
+const Stream = require('stream');
+const FormData = require('./FormData');
 
-/*Kick*/
-client.on('message',message =>{
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLocaleLowerCase() === prefix + 'kick'){
-       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
-       let member = message.mentions.members.first()
-       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
-       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
-       if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur :sunglass:")
-       member.kick()
-       message.channel.send("**"+member.user.username + '** a été exclu :white_check_mark:')
-    }
-});
- 
-/*Ban*/
-client.on('message',message =>{
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLocaleLowerCase() === prefix + 'ban'){
-       if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
-       let member = message.mentions.members.first()
-       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
-       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas bannir cet utilisateur :x:")
-       if (!member.bannable) return message.channel.send("Je ne peux pas bannir cet utilisateur :sunglass:")
-       message.guild.ban(member, {days: 7})
-       message.channel.send("**"+member.user.username + '** a été banni :white_check_mark:')
-    }
-});
+const Package = require('../../package.json');
 
-client.on("message", message =>{
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLowerCase() === prefix + "clear"){
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
-        let count = args[1]
-        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
-        if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
-        if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
-        message.channel.bulkDelete(parseInt(count) + 1)
-    }
- 
-    if (args[0].toLowerCase() === prefix + "mute"){
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
-        let member = message.mentions.members.first()
-        if (!member) return message.channel.send("Membre introuvable")
-        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
-        if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
-        let muterole = message.guild.roles.find(role => role.name === 'Muted')
-        if (muterole) {
-            member.addRole(muterole)
-            message.channel.send(member + ' a été mute :white_check_mark:')
-        }
-        else {
-            message.guild.createRole({name: 'Muted', permissions: 0}).then((role) => {
-                message.guild.channels.filter(channel => channel.type === 'text').forEach(channel => {
-                    channel.overwritePermissions(role, {
-                        SEND_MESSAGES: false
-                    })
-                })
-                member.addRole(role)
-                message.channel.send(member + ' a été mute :white_check_mark:')
-            })
-        }
-    }
-})
+const transports = {
+  'http:': http,
+  'https:': https,
+  'file:': require('./transports/file'),
+};
 
-const warns = JSON.parse(fs.readFileSync('./warns.json'))
- 
-client.on("message", message => {
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLowerCase() === prefix + "warn") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
-        let member = message.mentions.members.first()
-        if (!member) return message.channel.send("Veuillez mentionner un membre")
-        if (member.highestRole.comparePositionTo(message.member.highestRole) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas warn ce membre")
-        let reason = args.slice(2).join(' ')
-        if (!reason) return message.channel.send("Veuillez indiquer une raison")
-        if (!warns[member.id]) {
-            warns[member.id] = []
-        }
-        warns[member.id].unshift({
-            reason: reason,
-            date: Date.now(),
-            mod: message.author.id
-        })
-        fs.writeFileSync('./warns.json', JSON.stringify(warns))
-        message.channel.send(member + " a été warn pour " + reason + " :white_check_mark:")
+function buildRequest(method, url) {
+  /* istanbul ignore next */
+  this._read = () => {
+    this.resume();
+    if (this._response)
+      return;
+    this.catch((err) => this.emit('error', err));
+  };
+
+  this.options.lastBuiltUrl = url;
+
+  const options = URL.parse(url);
+  options.encoding = 'utf8';
+
+  if (!options.protocol)
+    throw new Error('URL must have a valid protocol');
+
+  const transport = transports[options.protocol];
+  options.method = method.toUpperCase();
+
+  if (this.options.headers)
+    options.headers = this.options.headers;
+
+  if (this.options.agent)
+    options.agent = this.options.agent;
+  else if (transport.Agent && this.options.followRedirects !== false)
+    options.agent = new transport.Agent({ keepAlive: true });
+
+  if (options.port)
+    options.port = parseInt(options.port);
+
+  this.options._req = options;
+  const request = transport.request(options);
+  if (request.setNoDelay)
+    request.setNoDelay(true);
+  return request;
+}
+
+function finalizeRequest() {
+  return new Promise((resolve, reject) => {
+    const request = this.request;
+
+    let socket;
+
+    const handleError = (err) => {
+      if (!err)
+        err = new Error('Unknown error occured');
+      err.request = request;
+      reject(err);
+      if (socket)
+        socket.removeListener('error', handleError);
+    };
+
+    request.once('abort', handleError);
+    request.once('error', handleError);
+    request.once('socket', (s) => {
+      socket = s;
+      s.once('error', handleError);
+    });
+
+    request.once('response', (response) => {
+      if (socket)
+        socket.removeListener('error', handleError);
+      let stream = response;
+      if (shouldUnzip(response)) {
+        stream = response.pipe(zlib.createUnzip({
+          flush: zlib.Z_SYNC_FLUSH,
+          finishFlush: zlib.Z_SYNC_FLUSH,
+        }));
+      }
+
+      if (this.options.followRedirects !== false && [301, 302, 303, 307, 308].includes(response.statusCode)) {
+        resolve({
+          response,
+          redirect: URL.resolve(this.options.lastBuiltUrl, response.headers.location),
+        });
+        response.destroy();
+      } else {
+        const body = [];
+
+        stream.on('data', (chunk) => {
+          if (!this.push(chunk))
+            this.pause();
+          body.push(chunk);
+        });
+
+        stream.once('end', () => {
+          this.push(null);
+          const raw = Buffer.concat(body);
+          resolve({ response, raw, redirect: false });
+        });
+      }
+    });
+
+    if (!this.request.getHeader('user-agent'))
+      this.set('User-Agent', `snekfetch/${Package.version} (${Package.homepage})`);
+
+    this._finalizeRequest();
+    let data = this.data;
+    if (data && data.end)
+      data = data.end();
+    if (Array.isArray(data)) {
+      for (const chunk of data)
+        request.write(chunk);
+      request.end();
+    } else if (data instanceof Stream) {
+      data.pipe(request);
+    } else if (data instanceof Buffer) {
+      request.end(data);
+    } else if (data) {
+      request.end(data);
+    } else {
+      request.end();
     }
- 
-    if (args[0].toLowerCase() === prefix + "infractions") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
-        let member = message.mentions.members.first()
-        if (!member) return message.channel.send("Veuillez mentionner un membre")
-        let embed = new Discord.RichEmbed()
-            .setAuthor(member.user.username, member.user.displayAvatarURL)
-            .addField('10 derniers warns', ((warns[member.id]) ? warns[member.id].slice(0, 10).map(e => e.reason) : "Ce membre n'a aucun warns"))
-            .setTimestamp()
-        message.channel.send(embed)
-    }
-})
+  });
+}
+
+function shouldSendRaw(data) {
+  return data instanceof Buffer || data instanceof Stream;
+}
+
+function shouldUnzip(res) {
+  if (res.statusCode === 204 || res.statusCode === 304)
+    return false;
+  if (res.headers['content-length'] === '0')
+    return false;
+  return /^\s*(?:deflate|gzip)\s*$/.test(res.headers['content-encoding']);
+}
+
+module.exports = {
+  buildRequest, finalizeRequest, shouldSendRaw,
+  METHODS: http.METHODS,
+  STATUS_CODES: http.STATUS_CODES,
+  FormData,
+  Extension: Stream.Readable,
+};
